627b421c9b13fe96f7f58305129a253fabc40ecf
leejane07 <leejane07@gmail.com>
2017-08-04 15:33:13 -0400

    cleaning up code

--- controllers/studybuddy_controller.js
+++ controllers/studybuddy_controller.js
@@ -1,21 +1,5 @@
+
 var db = require("../models");
-//var jsonUser = require("../public/js/app.js");
 
 module.exports = function(app) {}
 	
-/*//POST route that saves a new post to the database
-	app.post("/questions", function(req,res){
-		console.log(req.body);
-		db.User.create({
-			username: req.body.userName,
-			language: req.body.language,
-			rating: req.body.rating
-		})
-		.then(function(dbPost){
-			res.json(dbPost);
-			console.log(dbPost)
-		});
-	});
-};
-
-*/
