1d1c3985e1908ecf08a1cf15ca61dd1f5f2278e8
Jane Lee <janelee@Janes-MacBook-Pro-2.local>
2017-08-03 23:46:02 -0400

    edited app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,4 @@
-console.log("i'm loading");
-
-let form = $("#form");
-let language = $("#language option:selected"); 
-let rating = $("#rating option:selected");
-let userName = $("#userName");
-
-
-
-//write event handler for submit button that pushes form results into the db using post request in controllers.
-$(form).on("submit", function handleFormSubmit(event){
-	event.preventDefault();
-
-	//creates an object which contains form data
-	let newUser = {
-		username: userName.val().trim(),
-		language: language.text(),
-		rating: rating.text()
-	};
-
-	$.post('/questions', newUser)
-	 .done(console.log)
-	 .fail(console.error);
-
-	let jsonUser = JSON.stringify(newUser);
-	console.log(jsonUser)
-	console.log(JSON.stringify(newUser));
-});
-
+$(document).ready(function(){
+// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+$('.modal-trigger').leanModal();
+}); 
